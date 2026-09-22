21dbb08adbd2b9fe6bf70c3cb1583f8f1feb1973
Vlad Senchuk <senchukovvlad@gmail.com>
2026-09-22 18:01:33 +0300

    Add farewell feature

--- src/devops_lab1/main.py
+++ src/devops_lab1/main.py
@@ -15,3 +15,22 @@ def main():
 
 if __name__ == "__main__":
   main()
+  """Головний модуль програми."""
+
+  from devops_lab1.lib import calculate_length, farewell, greet
+
+
+  def main():
+    """Головна функція для демонстрації імпорту з lib.py."""
+    user = "Vlad"
+    message = greet(user)
+    length = calculate_length(message)
+    bye_message = farewell(user)
+
+    print(message)
+    print(f"Кількість символів у повідомленні: {length}")
+    print(bye_message)
+
+
+  if __name__ == "__main__":
+    main()
